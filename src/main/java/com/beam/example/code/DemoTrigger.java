87fc2687cadf53d38bf21703527288663ff9090b
sdut406 <1174822726@qq.com>
2020-09-18 18:25:54 +0800

    添加读取kafka数据demo

--- src/main/java/com/beam/example/code/DemoTrigger.java
+++ src/main/java/com/beam/example/code/DemoTrigger.java
@@ -1,8 +1,17 @@
 package com.beam.example.code;
 
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.kafka.KafkaIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.SerializableFunction;
+import org.apache.beam.sdk.transforms.windowing.AfterWatermark;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.values.KV;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.joda.time.Duration;
+import org.joda.time.Instant;
 
 /**
  * 触发器demo
@@ -10,10 +19,22 @@ import org.apache.beam.sdk.options.PipelineOptionsFactory;
  * @since 2020-09-18
  */
 public class DemoTrigger {
+    private static long TIME_NOW = 1600332600000L; // 基本时间
     public static void main(String[] args) {
         PipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().withoutStrictParsing().create();
         Pipeline pipeline = Pipeline.create(options);
-        
+        KafkaIO.Read<String, String> readKafka = KafkaIO.<String, String>read().withBootstrapServers("127.0.0.1:9092").withTopic("BeamTest")
+                .withKeyDeserializer(StringDeserializer.class)
+                .withValueDeserializer(StringDeserializer.class)
+                .withTimestampFn(new SerializableFunction<KV<String, String>, Instant>() {
+                    @Override
+                    public Instant apply(KV<String, String> input) {
+                        return new Instant(TIME_NOW + Integer.parseInt(input.getValue()));
+                    }
+                });
+        pipeline.apply(readKafka.withoutMetadata()).apply(Window.into(FixedWindows.of(Duration.standardSeconds(3)))
+                .withAllowedLateness(Duration.standardSeconds(5)).discardingFiredPanes()
+                .triggering(AfterWatermark.pastEndOfWindow().withLateFirings()))
         
     }
 }
